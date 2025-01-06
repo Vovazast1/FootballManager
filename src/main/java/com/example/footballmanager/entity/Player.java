3760c68ae8309ceb0daae99e47ebe6cabe10a517
Volodya <volodyazastavny@gmail.com>
2025-01-06 12:08:39 +0200

    Added lombok dependency and refactored entity classes

--- src/main/java/com/example/footballmanager/entity/Player.java
+++ src/main/java/com/example/footballmanager/entity/Player.java
@@ -2,10 +2,16 @@ package com.example.footballmanager.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.Period;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "players")
 public class Player {
@@ -28,74 +34,7 @@ public class Player {
     @JoinColumn(name = "team_id")
     private Team team;
 
-    public Player() {}
-
-    public Player(Long id, String name, LocalDate dob, Integer age, Integer experience, Team team) {
-        this.id = id;
-        this.name = name;
-        this.dob = dob;
-        this.age = age;
-        this.experience = experience;
-        this.team = team;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
     public Integer getAge() {
         return Period.between(this.dob, LocalDate.now()).getYears();
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Integer getExperience() {
-        return experience;
-    }
-
-    public void setExperience(Integer experience) {
-        this.experience = experience;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-
-    @Override
-    public String toString() {
-        return "Player{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", dob=" + dob +
-                ", age=" + age +
-                ", experience=" + experience +
-                ", team=" + team +
-                '}';
     }
 }
